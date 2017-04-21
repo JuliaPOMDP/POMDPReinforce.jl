c1c24ce6f9865bd9dead33bf634b706287532fd4
Maxim Egorov <maxgegorov@gmail.com>
2017-04-21 12:20:52 -0700

    changes to convert

--- src/POMDPReinforce.jl
+++ src/POMDPReinforce.jl
@@ -26,21 +26,26 @@ type MDPEnv <: AbstractEnvironment
     problem::MDP
     s::Array{Float64}
     r::Float64
+    t::Type
     rng::AbstractRNG
 end
-MDPEnv(p::MDP, rng::AbstractRNG=MersenneTwister()) = MDPEnv(p, convert(p, initial_state(p,rng)), 0.0, rng)
+function MDPEnv(p::MDP, rng::AbstractRNG=MersenneTwister()) 
+    s = initial_state(p,rng)
+    MDPEnv(p, convert(Array{Float64},initial_state(p,rng),p), 0.0, typeof(s), rng)
+end
 
 type POMDPEnv <: AbstractEnvironment
     problem::POMDP
     s
     o::Array{Float64}
     r::Float64
+    t::Type
     rng::AbstractRNG
 end
 function POMDPEnv(p::POMDP, rng::AbstractRNG=MersenneTwister()) 
     s = initial_state(p,rng)
-    o = convert(p, generate_o(p, s, rng))
-    return POMDPEnv(p, s, o, 0.0, rng)
+    o = convert(Array{Float64}, generate_o(p, s, rng), p)
+    return POMDPEnv(p, s, o, 0.0, typeof(o), rng)
 end
 
 type POMDPActionSpace
@@ -55,8 +60,8 @@ Base.in(a, as::POMDPActionSpace) = true
 Reinforce.ismdp(env::MDPEnv) = true
 Reinforce.ismdp(env::POMDPEnv) = false
 
-Reinforce.finished(env::MDPEnv, s) = isterminal(env.problem, convert(env.problem, s))
-Reinforce.finished(env::POMDPEnv, o) = isterminal(env.problem, env.s)
+finished(env::MDPEnv, s) = isterminal(env.problem, convert(env.t, s, env.problem))
+finished(env::POMDPEnv, o) = isterminal(env.problem, env.s)
 
 Reinforce.actions(env::MDPEnv, s) = POMDPActionSpace(POMDPs.actions(env.problem, s), env.rng)
 Reinforce.actions(env::POMDPEnv, s) = POMDPActionSpace(POMDPs.actions(env.problem, s), env.rng)
@@ -71,26 +76,26 @@ Reinforce.reward(env::POMDPEnv) = env.r
 
 function Reinforce.reset!(env::MDPEnv)
     s = initial_state(env.problem, env.rng)
-    env.s = convert(env.problem, s)
-    return convert(env.problem, s)
+    env.s = convert(Array{Float64}, s, env.problem)
+    return env.s
 end
 
 function Reinforce.reset!(env::POMDPEnv)
     s = initial_state(env.problem, env.rng)
     o = generate_o(env.problem, s, env.rng)
-    env.s, env.o = s, convert(env.problem, o)
+    env.s, env.o = s, convert(Array{Float64}, o, env.problem)
     return env.o
 end
 
 function Reinforce.step!(env::MDPEnv, s, a)
-    sp, r = generate_sr(env.problem, convert(env.problem, s), a, env.rng)
-    env.r, env.s = r, convert(env.problem, sp)
+    sp, r = generate_sr(env.problem, convert(env.t, s, env.problem), a, env.rng)
+    env.r, env.s = r, convert(Array{Float64}, sp, env.problem)
     return r, env.s
 end
 
 function Reinforce.step!(env::POMDPEnv, o, a)
     sp, op, r = generate_sor(env.problem, env.s, a, env.rng)
-    env.s, env.o, env.r = sp, convert(env.problem, op), r
+    env.s, env.o, env.r = sp, convert(Array{Float64}, op, env.problem), r
     return r, env.o
 end
 
